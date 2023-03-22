e037f808415e5ff0d0aa00e9d59bdfa769086d00
Oleksandr Khomiak <khomiak@gmail.com>
2023-03-22 09:41:47 +0200

    Done

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,19 @@
 //-------ФУНКЦІЯ ОТРИМАННЯ МАСИВУ КРАЇН -------//
 
 export default function fetchCountries(name) {
-  const url = `https://restcountries.com/v3.1/name/${name}`;
+  // лінк запиту
+  const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
 
+  // робимо чечез fetch get-запит на сервер, перетворюємо у формат json
+  // і повертаємо масив країн, які задовольняють запиту
+  // Якщо отримуємо  404, то повертаємо порожній масив
   return fetch(url)
-    .then(r => r.json())
+    .then(response => {
+      if (!response.ok) {
+        return [];
+      }
+      return response.json();
+    })
     .then(countryArray => {
       return countryArray;
     })
@@ -13,22 +22,4 @@ export default function fetchCountries(name) {
     });
 }
 
-//-------ФУНКЦІЯ ОТРИМАННЯ МАСИВУ КРАЇН -------//
-
-// export default function fetchCountries(name) {
-//     const url = `https://restcountries.com/v3.1/name/${name}`;
-// //   const url = `https://restcountries.com/v3.1/name/${name}?fullText=true`;
-
-//   fetch(url)
-//     .then(r => r.json())
-//     .then(countryArray => {
-//       return countryArray.forEach(country => {
-//         // if (country.name.common.toUpperCase().includes(name.toUpperCase())) {
-//         //   console.log(country.name.common);
-//         // }
-//           console.log(countryArray);
-//           //   showMessage(countryArray.length);
-
-//       });
-//     });
-// }
+// чомусь в консолі вибиває 404 помилку, якщо немає збігу. Так і має бути?
--- src/index.js
+++ src/index.js
@@ -1,41 +1,46 @@
 //-------ПІДКЛЮЧЕННЯ-------//
-// бібліотека виведення повідомлень
+// бібліотека виведення вспливаючих повідомлень
 import Notiflix from 'notiflix';
 // таблиця стилів
 import './css/styles.css';
-// імпорт запиту країни
+// імпорт функції запиту країни з API Rest Countries v2
 import fetchCountries from './fetchCountries';
-
-// import countryCard from './templates/country-card.hbs';
-
-//-------\-------//
-
+// бібліотека debounce затримка виконання коду
+import debounce from 'lodash.debounce';
+// затримка 300 мс
 const DEBOUNCE_DELAY = 300;
 
 //-------ЕЛЕМЕНИТ ІНПУТА-------//
 const searchBoxInput = document.querySelector('#search-box');
 const countryCard = document.querySelector('.country-info');
+const listOfCountries = document.querySelector('.country-list');
 
-//-------СЛУХАЧ ВВЕДЕНОГО ТЕКСТУ-------//
-searchBoxInput.addEventListener('input', onInputCountry);
+//-------СЛУХАЧ ВВЕДЕНОГО ТЕКСТУ НА ПОДІЮ ІНПУТ-------//
+// обробник викликається завдяки debounce через 300 мс
+searchBoxInput.addEventListener(
+  'input',
+  debounce(onInputCountry, DEBOUNCE_DELAY)
+);
 
-//-------ФУНКЦІЯ ОБРОБНИК -------//
+//-------ФУНКЦІЯ ОБРОБНИК ОТРИМАНОГО ЗНАЧЕННЯ З ІНПУТ-------//
 function onInputCountry(event) {
-  // введений текст в інпут
-  let inputText = event.currentTarget.value;
+  // введений текст в інпут трімимо (відкидаємо пробіли)
+  let inputText = event.target.value.trim();
+
+  // якщо запит порожній рядок, то виходимо
+  if (inputText === '') {
+    return;
+  }
 
   // вибираємо масив об"єктів країн які підходять критерію пошуку
   fetchCountries(inputText).then(countries => {
-    // console.log(countries);
+    // очищаємо поле переліку країн і картки окремої країни
+    hideCountryInfo();
+    hideCountryList();
 
-    //якщо країн більше 10 - виводимо повідомлення
-    if (countries.length > 10) {
-      showInformMessage();
-    }
-
-    //якщо країн 10 s менше - виводимо їх список
-    if (countries.length <= 10 && countries.length > 1) {
-      showCountryList(countries);
+    //Якщо збігів не виявлено ?????????????
+    if (countries.length === 0) {
+      showErrorMessage();
     }
 
     //якщо країна одна показуємо про неї дані
@@ -43,9 +48,14 @@ function onInputCountry(event) {
       showCountryInfo(countries[0]);
     }
 
-    //Якщо збігів не виявлено
-    if (countries.length === 0) {
-      showErrorMessage();
+    //якщо країн від 2 до 10, то виводимо їх список
+    if (countries.length <= 10 && countries.length > 1) {
+      showCountryList(countries);
+    }
+
+    //якщо країн більше 10 - виводимо повідомлення звузити пошук
+    if (countries.length > 10) {
+      showInformMessage();
     }
   });
 }
@@ -53,29 +63,70 @@ function onInputCountry(event) {
 //-------ФУНКЦІЯ ВИВОДУ ПОВІДОМЛЕННЯ, ЩО ЗАБАГАТО КРАЇН -------//
 function showInformMessage() {
   Notiflix.Notify.info(
-    'Too many matches found. Please enter a more specific name.'
+    'Too many matches found. Please enter a more specific name.',
+    {
+      timeout: 700,
+    }
   );
 }
 
 //-------ФУНКЦІЯ ВИВОДУ ПОВІДОМЛЕННЯ, ЩО НЕМАЄ КРАЇН ЗІ ЗБІГОМ -------//
 function showErrorMessage() {
-  Notiflix.Notify.failure('Oops, there is no country with that name');
+  Notiflix.Notify.failure('Oops, there is no country with that name', {
+    timeout: 700,
+  });
 }
 
-//-------ФУНКЦІЯ ВИВОДУ ПЕРЕЛІКУ КРАЇН -------//
+//-------ФУНКЦІЯ ВИВОДУ ПЕРЕЛІКУ КРАЇН ДЛЯ КОРИСТУВАЧА -------//
 function showCountryList(countries) {
   console.log(countries);
+
+  // формуємо верстку
+  let message = countries
+    .map(country => {
+      return `<li class="list">
+    <div class="country-card-all country-card">
+    <img class="country-flag" src="${country.flags.svg}" 
+        alt="${country.name.official} flag"> 
+        <p2>${country.name.official}</p2>
+        </div>
+        </li>`;
+    })
+    .join('');
+
+  // вставляємо верстку в макет
+  listOfCountries.innerHTML = message;
 }
 
-//-------ФУНКЦІЯ ВИВОДУ ДАНИХ ПРО ОДНУ КРАЇНУ -------//
+//-------ФУНКЦІЯ ВИВОДУ ДАНИХ ПРО ОДНУ КРАЇНУ ДЛЯ КОРИСТУВАЧА-------//
 function showCountryInfo(country) {
-  console.log(country.name.official);
-  console.log(country.capital);
-  console.log(country.population);
-  console.log(country.flags.svg);
-  console.log(country.languages);
+  console.log(country);
 
-  let countryInfo = `Country: ${country.name.official}`;
+  // витягаємо з обʼєкту значення офіційних мов країни
+  let languages = Object.values(country.languages);
 
-  countryCard.insertAdjacentHTML('beforeend', countryInfo);
+  // формуємо верстку
+  let countryInfo = `<div class="country-card-all">
+      <div class="country-card">
+        <img class="country-flag" src="${country.flags.svg}" 
+        alt="${country.name.official} flag">
+        <h2 class="country-official-name">${country.name.official}</h2>
+      </div>
+      <p><strong>Capital:</strong> ${country.capital}</p>
+      <p><strong>Population:</strong> ${country.population}</p>
+      <p><strong>Languages:</strong> ${languages}</p>
+    </div>`;
+
+  // вставляємо верстку в макет
+  countryCard.innerHTML = countryInfo;
+}
+
+//-------ФУНКЦІЯ ОЧИЩЕННЯ ПЕРЕЛІКУ КРАЇН ДЛЯ КОРИСТУВАЧА-------//
+function hideCountryList() {
+  listOfCountries.innerHTML = '';
+}
+
+//-------ФУНКЦІЯ ОЧИЩЕННЯ ДАНИХ ПРО ОДНУ КРАЇНУ ДЛЯ КОРИСТУВАЧА-------//
+function hideCountryInfo() {
+  countryCard.innerHTML = '';
 }
